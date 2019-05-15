a2ad8602be7f43d6d6707b97109221f72919b10b
Alejandro Cordero <a.cordero@students.chapelfield.org>
2019-05-15 08:32:04 -0400

    Add mad libs game

---
+++ Mad Libs.py
@@ -0,0 +1,8 @@
+adjective = raw_input("Enter an adjective: ")
+noun =raw_input("Enter a noun: ")
+verb =raw_input("Enter a verb: ")
+punctuation = raw_input("Enter a punctuation mark: ") 
+print ("MAD LIBS")
+print ("The " + adjective + " " + noun + " " + verb + " over the big black horse" + punctuation) 
+ 
+
